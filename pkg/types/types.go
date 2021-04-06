a72b75f4a225cc163b8f9ff89aa77341584ed3c7
Kurbonali <kurbon.ismailov@gmail.com>
2021-04-06 17:05:13 +0500

    need

---
+++ pkg/types/types.go
@@ -0,0 +1,41 @@
+package types
+
+type Money int64
+type Currency string
+
+const (
+	StatusOk Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"	
+
+)
+
+type PAN string
+type Status string
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+
+type Payment struct {
+
+	ID       	int
+	Amount   	Money
+	Category 	Category
+	Status		Status
+}
+
+// type of sourses of payment for choosing
+type PaymentSource struct {
+	Type    string // 'card'
+	Number  string // PAN number
+	Balance Money  // balance in dirams
+}
+
+//Категории платежей
+type Category string
